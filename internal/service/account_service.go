b6e4af48337cc6afad9ebb2c7063c74d327de002
Patricia Ferrer <patricia.ferreira@esx.com.br>
2025-04-08 10:17:12 -0300

    feat: função no service para criar uma conta

--- internal/service/account_service.go
+++ internal/service/account_service.go
@@ -1,7 +1,10 @@
 // service permite que eu tenha os métodos para criar e pegar os dados das contas
 package service
 
-import "github.com/patsferrer/go-gateway/internal/domain"
+import (
+	"github.com/patsferrer/go-gateway/internal/domain"
+	"github.com/patsferrer/go-gateway/internal/dto"
+)
 
 type AccountService struct {
 	// usa a interface para acessar o banco de dados
@@ -13,6 +16,30 @@ func NewAccountService(repository domain.AccountRepository) *AccountService {
 	return &AccountService{repository: repository}
 }
 
+// função para criar uma conta
+func (s *AccountService) CreateAccount(input *dto.CreateAccountInput) (*dto.AccountOutput, error) {
 
+	// converte o dto em um domínio
+	account := dto.ToAccount(input)
 
+	// verifica se a conta já existe baseado na APIKey
+	existingAccount, err := s.repository.FindByAPIKey(account.APIKey)
 
+	if err != nil && err != domain.ErrAccountNotFound {
+		return nil, err
+	}
+
+	// se a conta já existe e não é um erro de conta não encontrada, retorna um erro
+	if existingAccount != nil {
+		return nil, domain.ErrDuplicateAPIKey
+	}
+
+	// salva a account no banco de dados
+	err = s.repository.Save(account)
+	if err != nil {
+		return nil, err
+	}
+
+	output := dto.FromAccount(account)
+	return output, nil
+}
